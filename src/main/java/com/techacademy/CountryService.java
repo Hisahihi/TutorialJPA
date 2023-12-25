7b82faa307921cf88d1c3aac29deecf3c60e0519
HIsahihi <jiuqingshui47@gmail.com>
2023-12-25 18:22:04 +0900

    追加　更新　削除追記

--- src/main/java/com/techacademy/CountryService.java
+++ src/main/java/com/techacademy/CountryService.java
@@ -1,9 +1,11 @@
 package com.techacademy;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CountryService {
@@ -18,6 +20,24 @@ public class CountryService {
 	public List<Country> getCountryList(){
 		//リポジトリのFindAllを呼び出し
 		return repository.findAll();
+	}
+
+	//一件検索
+	public Country getCountry(String code) {
+		Optional <Country> option = repository.findById(code);
+		Country country = option.orElse(null);
+		return country;
+	}
+	//更新（追加）を行う
+	@Transactional
+	public void update(String code, String name, int population) {
+		Country country = new Country(code,name,population);
+		repository.save(country);
+	}
+	//削除する
+	@Transactional
+	public void deleteCountry(String code) {
+		repository.deleteById(code);
 	}
 
 }
